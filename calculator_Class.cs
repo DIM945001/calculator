558192ae43c0c4cb73bebf3c98b1830a60ad1aae
dimich <dimich@DESKTOP-MQE6IFE>
2021-10-11 10:19:44 +0400

    Добавьте файлы проекта.

---
+++ calculator_Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class calculator_Class
+    {
+        public int Summa(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int minus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
